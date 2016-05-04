0c11a0d2727d20b2b0398e231a7ccc01959326df
Kreat1vK0dr <daniel@projectcodex.co>
2016-05-04 17:42:28 +0200

    added database setup files.

---
+++ database-setup/database-setup.sql
@@ -0,0 +1,47 @@
+# NOTE: RUN $ mysql --local-infile -u <user> -p BEFORE EXECUTING THIS SCRIPT
+DROP DATABASE IF EXISTS mqabaqaba;
+CREATE DATABASE mqabaqaba;
+#CREATE USER user@localhost IDENTIFIED BY 'password';
+#GRANT ALL PRIVILEGES ON mqabaqaba.* TO user@localhost;
+#FLUSH PRIVILEGES;
+
+USE mqabaqaba;
+
+CREATE TABLE users(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(40)
+  #password VARCHAR(100),
+);
+
+INSERT INTO users (name) VALUES ("lusando"),("eugene");
+
+CREATE TABLE challenge_types(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  description VARCHAR(20)
+);
+
+INSERT INTO challenge_types (description) VALUES ("social"),("exercise"),("both");
+
+CREATE TABLE user_challenges(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  user_id INT,
+  date DATE,
+  time TIME,
+  challenge_id INT,
+  recurring INT
+);
+
+CREATE TABLE  challenges(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  description VARCHAR(1000),
+  type_id INT,
+  time_limit INT,
+  distance INT
+  #requirements VARCHAR(1000)
+);
+
+LOAD DATA LOCAL INFILE "../data/challenges.csv" INTO TABLE challenges
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(description, type_id, time_limit, distance);
