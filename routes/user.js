f6147cc5e16acdfb21de46b786f63320e7c864bd
Kreat1vK0dr <daniel@projectcodex.co>
2016-05-04 22:21:46 +0200

    added some images for user selection. added a view for user-home and for when the user adds a challenge. Added a bit of css.

---
+++ routes/user.js
@@ -0,0 +1,33 @@
+exports.showChallenge = function(req, res, next) {
+  var id = req.params.userID,
+      context;
+
+      req.getConnection(function(err, connection){
+        connection.query("SELECT * from user_challenges WHERE user_id = ?", id, function(err, result){
+          if (err) return next(err);
+          context = {
+                      no_challenges: result.length===0,
+                      challenges: result.length>0,
+                      userID: id
+          };
+          connection.query("SELECT * from users WHERE id = ?", id, function(err, result){
+            if (err) return next(err);
+            context.user = result[0].name;
+
+            res.render("user-home", context);
+          });
+        });
+      });
+};
+
+exports.addChallenge = function (req, res, next) {
+  var id = req.params.userID,
+      type = req.body.type,
+      time = req.body.time,
+      date = Date.now();
+
+  connection.query("INSERT INTO user_challenges (user_id, challenge_type, date, time) VALUES ?", [id, type,date, time], function(err, result){
+    if (err) return next(err);
+    res.redirect('/home/'+id);
+  });
+};
