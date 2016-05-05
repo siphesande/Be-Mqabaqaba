bda77f556fee2b3873da021ebfde9746651e5a2a
Kreat1vK0dr <daniel@projectcodex.co>
2016-05-05 09:00:03 +0200

    configured time and challenge type input by user.

--- routes/user.js
+++ routes/user.js
@@ -23,11 +23,32 @@ exports.showChallenge = function(req, res, next) {
 exports.addChallenge = function (req, res, next) {
   var id = req.params.userID,
       type = req.body.type,
-      time = req.body.time,
-      date = Date.now();
+      timeArray = req.body.time.split(":"),
+      dateNow = new Date();
+console.log(timeArray);
+var challenge_hour = timeArray[0],
+    challenge_minute = timeArray[1];
 
-  connection.query("INSERT INTO user_challenges (user_id, challenge_type, date, time) VALUES ?", [id, type,date, time], function(err, result){
+  var day = dateNow.getDay() + 1,
+      month = dateNow.getMonth(),
+      year = dateNow.getFullYear();
+
+  var date = new Date(year,month,day),
+      challenge_time = new Date(year, month, day, challenge_hour, challenge_minute);
+  console.log(year, month, day, challenge_hour, challenge_minute);
+  console.log(challenge_time);
+
+var input = {
+            user_id: id,
+            challenge_id: type,
+            date: date,
+            time: challenge_time,
+            recurring: 0
+}
+      req.getConnection(function(err, connection){
+  connection.query("INSERT INTO user_challenges SET ?", input, function(err, result){
     if (err) return next(err);
-    res.redirect('/home/'+id);
+    res.redirect('/user/'+id);
   });
+});
 };
